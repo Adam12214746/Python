5b2bbd55df1b41f1d7e111e4face1241fa12bcf3
Adam <136097061+Adam12214746@users.noreply.github.com>
2025-04-12 09:04:40 +0900

    Create boolean.py

---
+++ boolean.py
@@ -0,0 +1,20 @@
+r1 = True
+r2 = True
+r3 = True
+r4 = False
+r5 = True
+#c
+print(type(r1))
+print(type(r2))
+print(type(r3))
+print(type(r4))
+print(type(r5))
+#d
+print(r1, r2, r3, r4, r5)
+#e
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+if num1 == num2: print("True")
+else: print("False")
+    
+### Feedback: Great!!!
