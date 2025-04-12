b26d6bdcb804cf214aee1c04165198b8b9708887
Adam <136097061+Adam12214746@users.noreply.github.com>
2025-04-12 09:08:05 +0900

    Create string.py

---
+++ string.py
@@ -0,0 +1,21 @@
+r1 = "korea"
+r2 = "uzbekistan"
+r3 = "amerika"
+r4 = "fransiya"
+r5 = "rossiya"
+#c
+print(type(r1))
+print(type(r2))
+print(type(r3))
+print(type(r4))
+print(type(r5))
+#d
+print(r1, r2, r3, r4, r5)
+#e
+ism = str(input("ismingizni kiriting: "))
+familiya = str(input("familiyangizni kiriting: "))
+print((ism +" "+ familiya).upper())
+print((ism +" "+ familiya).lower())
+print(((ism).upper() +" "+ familiya))
+
+# Feedback: Great!!!
