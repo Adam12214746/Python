f78fca2cbf76606a5f4ce674bcb0a5cc62b4e07f
Adam <136097061+Adam12214746@users.noreply.github.com>
2025-04-12 09:06:16 +0900

    Create indexing.py

---
+++ indexing.py
@@ -0,0 +1,22 @@
+#string = "sun'iy intellekt" 
+#print(string[:6]) 
+#print(string[7:16]) 
+#print(string[-9:])
+#print(string[-16:-9])
+#matn = input("matn: ") 
+#print(matn[0].upper() + matn[1:])
+#print(matn.capitalize()) #option b
+#matn = input("matnni kiriting: ")
+#indeks = int(input("indeks kiriting: "))
+#harf = input("harf kiriting: ")
+#print(matn[:indeks] + harf + matn[indeks+1:])
+#yangi_matn = matn[:indeks] + harf + matn[indeks + 1:]
+#print("yangi matn: ", yangi_matn)
+#matn = str(input("matn kiriitng: "))
+#print("kiritilgan matnning har bir ikkinchi harfi: ")
+#for i in range(1, len(matn), 2):
+    #print(matn[i])
+#matn = input("matn kiriting: ")
+#print("matnning oxirgi 3ta harfi: ", matn[-3:])
+
+### Feedback: Good!
