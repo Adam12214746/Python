5492ad8174f0260676db818578c8c00bc969ebab
Adam <136097061+Adam12214746@users.noreply.github.com>
2025-04-12 09:05:08 +0900

    Create float.py

---
+++ float.py
@@ -0,0 +1,20 @@
+import math
+r1 = 1.1
+r2 = 2.2
+r3 = 3.3
+r4 = 4.4
+r5 = 5.5
+#c
+print(type(r1))
+print(type(r2))
+print(type(r3))
+print(type(r4))
+print(type(r5))
+#d
+print(r1,r2,r3,r4,r5)
+#e
+radius = float(input("radius:"))
+diametr = radius * 2
+print("diametr:", 3.14 * (radius ** 2) )
+
+# Feedback: Great!!!
