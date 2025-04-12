8573a64f8e25a2a2dd8b3031bf04fb4207eeed2c
Adam <136097061+Adam12214746@users.noreply.github.com>
2025-04-12 09:06:44 +0900

    Create integer.py

---
+++ integer.py
@@ -0,0 +1,22 @@
+#b
+r1 = 1
+r2 = 2
+r3 = 3
+r4 = 4
+r5 = 5
+#c
+print(type(r1))
+print(type(r2))
+print(type(r3))
+print(type(r4))
+print(type(r5))
+#d
+print(r1,r2,r3,r4,r5)
+#e
+num1 = int(input("numb1: "))
+num2 = int(input("numb2: "))
+print("add:", num1+num2)
+print("subtract:", num1-num2)
+print("multiply:", num1*num2)
+print("divide:", num1/num2)
+# Feedback: Great!!!
