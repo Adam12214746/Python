919e6931923352e4a742a59d5ab29e3afe9993d0
Adam <136097061+Adam12214746@users.noreply.github.com>
2025-04-12 09:07:18 +0900

    Create math.py

---
+++ math.py
@@ -0,0 +1,64 @@
+#import math
+#a = float(input("Enter the first number: "))
+#b = float(input("Enter the second number: "))
+#qoshish = a + b
+#ayrish = a - b
+#kopaytrish = a * b
+#bolish = a / b if b != 0 else "bolib bomaydi"
+#kvadrat_a = a ** 2
+#kvadrat_b = b ** 2
+#a_ni_b_ga = a ** b
+#ildiz = math.sqrt(a_ni_b_ga)
+#print("qoshish: ", qoshish)
+#print("ayrish: ", ayrish)
+#print("bolish: ", bolish)
+#print("a ning kvadrati: ", kvadrat_a)
+#print("b ning kvadrati: ", kvadrat_b)
+#print("a ni b ga oshirilgani: ", a_ni_b_ga)
+#print("ildiz: ", ildiz)
+###########################################
+#import math
+#a = float(input("Enter the first number: "))
+#b = float(input("Enter the second number: "))
+#a += b
+#print(a)
+#a -= b
+#print(a)
+#a *= b
+#print(a)
+#a /= b
+#print(a)
+##############################
+#import math
+#a = int(input("qoldiqli butun manfiy son yozing: "))
+#while a % 2 == 0:
+ #   a //= 2
+#b = -a
+#print("musbat butun qismi: ", b)
+#############################
+#import math
+#a = float(input("raqam kiriting: "))
+#b = float(input("raqam kiriting: "))
+#c = (a ** 2) + (b ** 2)
+#print("brinchi va ikkinchi sonning kvadratlarining qoshilgani: ", c)
+######################
+#from math import factorial
+#raqam = int(input("Enter raqam number: "))
+#factorial(raqam)
+#print(factorial(raqam))
+#############################
+#import math
+#from math import factorial
+#raqam = int(input("Enter the raqam: "))
+#factorial(raqam)
+#print(factorial(raqam))#1
+#ildiz = math.sqrt(raqam)
+#print("ildiz: ", ildiz)#2
+#kvadrat = math.pow(raqam, 2)
+#print("kvadrat: ", kvadrat)#3
+#absolyut = math.fabs(raqam)
+#print("absolyut: ", absolyut)#4
+#ceiling = math.ceil(raqam)
+#print("ceiling: ", ceiling)#5
+
+### Feedback: Good!
